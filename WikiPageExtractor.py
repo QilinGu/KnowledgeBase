1cc78d663c9279248223d3499f55390075cca4a0
ezio <zhhuab123456@gmail.com>
2015-03-12 21:58:07 +0800

    modify page 01

--- WikiPageExtractor.py
+++ WikiPageExtractor.py
@@ -1,7 +1,22 @@
+import re
+import xml.etree.ElementTree as etree
+
 class WikiPageExtractor:
 
-    def extract(filename):
-        pass
+    def extract(self, filename):
+        page = ""
+        with open(filename) as file:
+            for line in file:
+                if line == "  <page>\n":
+                    page = ""
+                page += line
+                if line == "  </page>\n":
+                    yield page
 
 if __name__ == "__main__":
-    pass
+    base_path = "/home/ezio/filespace/data/"
+    xml_path = base_path + "sample.xml"
+    extractor = WikiPageExtractor()
+    for page in extractor.extract(xml_path):
+        print(page)
+        print("\n\n\n\n\n\n\n\n\n\n\n\n")
