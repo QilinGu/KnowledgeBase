d1ad19ce53faa4e9d3ce4698f10ce0448a26ccc6
ezio <zhhuab123456@gmail.com>
2015-03-13 10:17:13 +0800

    text page modify

--- WikiPageExtractor.py
+++ WikiPageExtractor.py
@@ -5,16 +5,22 @@ class WikiPageExtractor:
 
     def extract(self, filename):
         page = ""
+        drop = False
         with open(filename) as file:
             for line in file:
                 if line == "  <page>\n":
+                    drop = False
                     page = ""
-                page += line
-                if line == "  </page>\n":
+                if line[:21] == "    <title>Wikipedia:" or line[:16] == "    <title>Help:":
+                    drop= True
+                if drop == False:
+                    page += line
+                if line == "  </page>\n" and drop == False:
                     yield page
 
 if __name__ == "__main__":
     base_path = "/home/ezio/filespace/data/"
+    # xml_path = base_path + "zhwiki-20140508-pages-articles-multistream.xml"
     xml_path = base_path + "sample.xml"
     extractor = WikiPageExtractor()
     for page in extractor.extract(xml_path):
--- WikiTextExtractor.py
+++ WikiTextExtractor.py
@@ -1,7 +1,22 @@
+import xml.etree.ElementTree as etree
+from WikiPageExtractor import WikiPageExtractor
+
 class WikiTextExtractor:
 
-    def extract(page):
-        pass
+    def extract(self, page):
+        page_node = etree.fromstring(page)
+        text_node = list(page_node.iter('text'))[0]
+        text = text_node.text
+        return text
 
 if __name__ == "__main__":
-    pass
+    base_path = "/home/ezio/filespace/data/"
+    # xml_path = base_path + "zhwiki-20140508-pages-articles-multistream.xml"
+    xml_path = base_path + "sample.xml"
+
+    extractor = WikiPageExtractor()
+    page_extractor = WikiPageExtractor()
+    text_extractor = WikiTextExtractor()
+    for page in page_extractor.extract(xml_path):
+        print(text_extractor.extract(page))
+        break
