2f75b6262535a1ef8b64c0ee8f19c16a6c67399f
ezio <zhhuab123456@gmail.com>
2015-03-31 19:59:33 +0800

    单线程访问语言云v2

--- WikiEntityRecognizer.py
+++ WikiEntityRecognizer.py
@@ -11,16 +11,19 @@ class WikiEntityRecognizer:
 
     def recognize(self, sent):
         url = "%sapi_key=%s&text=%s&format=%s&pattern=%s" % (self.base_url, self.api_key, sent, self.result_format, self.pattern)
-        try:
-            result = requests.get(url)
-        except:
-            print("get url raise an exception!")
-            print(traceback.format_exc())
-            return "### 获取url异常！"
-        if result.status_code ==  200:
-            return result.text
-        else:
-            return "### 错误响应" + str(result.status_code) + "！！！"
+        try_count = 3
+        while try_count > 0:
+            try_count -= 1
+            try:
+                result = requests.get(url)
+            except:
+                continue
+            if result.status_code !=  200:
+                continue
+            else:
+                return result.text
+        if try_count == 0:
+            return "### can't get url!!!"
 
 if __name__ == "__main__":
     input_file_path = "/home/ezio/filespace/data/plain_sentences.txt"
@@ -29,11 +32,14 @@ if __name__ == "__main__":
     output_file = open(output_file_path, 'w', 1000)
     recognizer = WikiEntityRecognizer()
 
-    i = 0
+    total_count = 0
+    fail_count = 0
     for line in input_file:
         sent = line.strip()
         ner_sent = recognizer.recognize(sent)
-        # output_file.write(ner_sent + '\n')
-        print(ner_sent[0: min(len(ner_sent), 10)])
-        i += 1
-        print(i)
+        if ner_sent == "### can't get url!!!": fail_count += 1
+        total_count += 1
+        print(total_count, end = ' ')
+        print(fail_count, end = ' ')
+        print(ner_sent[0: min(len(ner_sent), 20)])
+        output_file.write(ner_sent + '\n')
