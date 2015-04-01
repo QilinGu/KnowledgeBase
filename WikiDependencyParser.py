ead306411f32c6835af38d9b24a448a1f07e4dc2
ezio <zhhuab123456@gmail.com>
2015-04-01 22:27:52 +0800

    add WikiDependencyParser.py

--- WikiDependencyParser.py
+++ WikiDependencyParser.py
@@ -1,15 +1,15 @@
 import requests
 import traceback
 
-class WikiEntityRecognizer:
+class WikiDependencyParser:
 
     def __init__(self):
         self.base_url = "http://ltpapi.voicecloud.cn/analysis/?"
         self.api_key = "A3Q1y6d97QjSAzKAIaFLlN5LXItLoP2MpzAZLH6z"
-        self.pattern = "ner"
+        self.pattern = "dp"
         self.result_format = "plain"
 
-    def recognize(self, sent):
+    def parse(self, sent):
         url = "%sapi_key=%s&text=%s&format=%s&pattern=%s" % (self.base_url, self.api_key, sent, self.result_format, self.pattern)
         try_count = 3
         while try_count > 0:
@@ -26,22 +26,22 @@ class WikiEntityRecognizer:
             return "### can't get url!!!"
 
 if __name__ == "__main__":
-    input_file_path = "/home/ezio/filespace/data/plain_sentences.txt"
-    output_file_path = "/home/ezio/filespace/data/ner_sentences.txt"
+    input_file_path = "/home/ezio/filespace/data/backup/plain_sentences.txt"
+    output_file_path = "/home/ezio/filespace/data/dp_sentences.txt"
     input_file = open(input_file_path, 'r')
     output_file = open(output_file_path, 'w', 1000)
-    recognizer = WikiEntityRecognizer()
+    parser = WikiDependencyParser()
 
     total_count = 0
     fail_count = 0
     for line in input_file:
         sent = line.strip()
-        ner_sent = recognizer.recognize(sent)
-        ner_sent = "".join(ner_sent.split('\n'))
-        if ner_sent == "### can't get url!!!":
+        dp_sent = parser.parse(sent)
+        dp_sent = "".join(dp_sent.split('\n'))
+        if dp_sent == "### can't get url!!!":
             fail_count += 1
         total_count += 1
         print(total_count, end = ' ')
         print(fail_count, end = ' ')
-        print(ner_sent[0: min(len(ner_sent), 20)])
-        output_file.write(ner_sent + '\n')
+        print(dp_sent[0: min(len(dp_sent), 20)])
+        output_file.write(dp_sent + '\n')
