3419ca16c04274bd6847203bb7a1feedfa217f21
ezio <zhhuab123456@gmail.com>
2015-04-03 23:55:15 +0800

    training/testing set ready, feature functions not ready

---
+++ feature_functions.py
@@ -0,0 +1,4 @@
+def f1(pos_sent, ner_sent, dpt_sent, reverse):
+    return 1
+
+fea_funcs = [f1, ]
--- re_init_work.py
+++ re_init_work.py
@@ -1,13 +1,17 @@
-import sys
+import re
 import random
 import pickle
+from feature_functions import fea_funcs
+import numpy
 
 def random_select_seed():
-    for i, line in enumerate(open('/home/ezio/filespace/data/ner_sentences.txt')):
-        if random.randint(0, 1000) == 1:
-            print(i + 1, end = ' ')
-            print(line, end = '')
-            sys.stdout.flush()
+    sent_list = pickle.load(open('/home/ezio/filespace/data/re_sent_list.data', 'rb'))
+    with open('/home/ezio/filespace/data/training_seeds.txt', 'w') as f:
+        for i, sent_dict in enumerate(sent_list):
+            if random.randint(0, 100) == 1:
+                if 'Ns' not in sent_dict['ner'] and random.randint(0, 1) != 0: continue
+                f.write(str(i))
+                f.write(' ' + sent_dict['ner'] + '\n')
 
 def dump_sent_list():
     pos_file  = open('/home/ezio/filespace/data/pos_sentences.txt' , 'r', 1000)
@@ -15,19 +19,98 @@ def dump_sent_list():
     dpt_file  = open('/home/ezio/filespace/data/dpt_sentences.txt' , 'r', 1000)
     dump_file = open('/home/ezio/filespace/data/re_sent_list.data', 'wb')
 
-    sent_list = [None]
+    sent_list = []
     while True:
-        pos_sent = pos_file.readline()
-        ner_sent = ner_file.readline()
-        dpt_sent = dpt_file.readline()
+        pos_sent = pos_file.readline().strip()
+        ner_sent = ner_file.readline().strip()
+        dpt_sent = dpt_file.readline().strip()
         if pos_sent == '' or ner_sent == '' or dpt_sent == '': break
+        if pos_sent == "### can't get url!!!" or ner_sent == "### can't get url!!!" or dpt_sent == "### can't get url!!!": continue
+        if re.search(r'\[.*\].*\[.*\]', ner_sent) == None: continue
         sent_list.append({'pos': pos_sent, 'ner': ner_sent, 'dpt': dpt_sent})
     pickle.dump(sent_list, dump_file)
     dump_file.close()
+    return sent_list
 
-def get_features(pos_sent, ner_sent, dpt_sent):
-    no_feature = False
-    if ner_sent == "### can't get url!!!": no_feature = True
-    if dpt_sent == "### can't get url!!!": no_feature = True
-    if re.search(r'\[.*\].*\[.*\]', ner_sent) == None: no_feature = True
+def pairwise_ner(ner_sent):
+    pattern = '\[.*?\]N[a-z]'
+    nes = re.findall(pattern, ner_sent)
+    nes = [''.join(ne.split()) for ne in nes]
+    sps = re.split(pattern, ner_sent)
+    nes_stripped = [ne.split(']')[0].split('[')[1] for ne in nes]
+    ne_count = len(nes)
+    pairwise_ner_sents = []
+    ne_pairs = []
+    for i in range(ne_count):
+        for j in range(i + 1, ne_count):
+            sent = ""
+            for k in range(ne_count):
+                sent += sps[k]
+                if k == i or k == j:
+                    sent += nes[k]
+                else:
+                    sent += nes_stripped[k]
+            pairwise_ner_sents.append(sent)
+            ne_pair = (nes_stripped[i], nes_stripped[j])
+            ne_pairs.append(ne_pair)
+    return pairwise_ner_sents, ne_pairs
 
+def sent_triple_features(pos_sent, ner_sent, dpt_sent, reverse):
+    return [func(pos_sent, ner_sent, dpt_sent, reverse) for func in fea_funcs]
+
+def sent_dict_features(sent_dict, lino):
+    pos_sent = sent_dict['pos']
+    dpt_sent = sent_dict['dpt']
+    pairwise_ner_sents, ne_pairs = pairwise_ner(sent_dict['ner'])
+    fea_vecs = []
+    for ner_sent, pair in zip(pairwise_ner_sents, ne_pairs):
+        raw_fea_vec = sent_triple_features(pos_sent, ner_sent, dpt_sent, reverse = False)
+        fea_vec = [lino, pair[0], pair[1]] + raw_fea_vec
+        fea_vecs.append(fea_vec)
+        raw_fea_vec = sent_triple_features(pos_sent, ner_sent, dpt_sent, reverse = True)
+        fea_vec = [lino, pair[1], pair[0]] + raw_fea_vec
+        fea_vecs.append(fea_vec)
+    return fea_vecs
+
+def sent_list_features(sent_list):
+    all_fea_vecs = []
+    for lino, sent_dict in enumerate(sent_list):
+        fea_vecs = sent_dict_features(sent_dict, lino)
+        all_fea_vecs.extend(fea_vecs)
+    return all_fea_vecs
+
+def boostrap_init_data():
+    sent_list = pickle.load(open('/home/ezio/filespace/data/re_sent_list.data', 'rb'))
+    all_fea_vecs = sent_list_features(sent_list)
+
+    training_dict = {}
+    for line in open('/home/ezio/filespace/data/training_seeds.txt'):
+        splited_line = line.strip().split('#')
+        if len(splited_line) == 2:
+            lino = int(splited_line[1].split()[0])
+            pair_list = [(pair.split('@')[0], pair.split('@')[1]) for pair in splited_line[0].split()]
+            pair_set = set(pair_list)
+        else:
+            lino = int(splited_line[0].split()[0])
+            pair_set = set()
+        training_dict[lino] = pair_set
+
+    training_list = []
+    testing_list = []
+    for fea_vec in all_fea_vecs:
+        lino = fea_vec[0]
+        if lino in training_dict:
+            if (fea_vec[0], fea_vec[1]) in training_dict[lino]:
+                training_list.append(fea_vec + [1])
+            else:
+                training_list.append(fea_vec + [0])
+        else:
+            testing_list.append(fea_vec + [-1])
+
+    training_set = numpy.asarray(training_list)
+    testing_set = numpy.asarray(testing_list)
+    pickle.dump(training_set, open('/home/ezio/filespace/data/training_set.data', 'wb'))
+    pickle.dump(testing_set, open('/home/ezio/filespace/data/testing_set.data', 'wb'))
+    return training_set, testing_set
+
+training_set, testing_set = boostrap_init_data()
