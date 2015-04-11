9ced8ce75398abd5467f1e8d1074c6bd4b2b856b
ezio <zhhuab123456@gmail.com>
2015-04-11 22:17:35 +0800

    balance train

--- RelationExtractor.py
+++ RelationExtractor.py
@@ -1,14 +1,20 @@
 import pickle
-# import time
+import time
 import numpy
-import random
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure import FeedForwardNetwork, FullConnection
 from pybrain.structure import LinearLayer, SigmoidLayer
+#from sklearn import svm
 
 class NeuralNetwork():
-    def __init__(self, in_count, hidden_counts, out_count):
+
+    def __init__(self, hidden_counts, training_list, learning_rate, balance_num):
+        self.training_list = training_list
+        self.set_network(len(training_list[0]) - 4, hidden_counts, 1)
+        self.set_trainer(learning_rate, balance_num)
+
+    def set_network(self, in_count, hidden_counts, out_count):
         assert len(hidden_counts) > 0
         self.in_count = in_count
         self.out_count = out_count
@@ -27,11 +33,30 @@ class NeuralNetwork():
         self.net.addConnection(out_connection)
         self.net.sortModules()
 
-    def set_trainer(self, ds, learning_rate):
-        self.trainer = BackpropTrainer(self.net, ds, learningrate = learning_rate)
+    def set_trainer(self, learning_rate, balance_num):
+        self.origin_ds = SupervisedDataSet(self.in_count, self.out_count)
+        self.balance_ds = SupervisedDataSet(self.in_count, self.out_count)
+        for sample in self.training_list:
+            feature = sample[3:-1]
+            label = sample[-1]
+            self.origin_ds.addSample(feature, label)
+            if label == 1:
+                for i in range(balance_num): self.balance_ds.addSample(feature, label)
+            else:
+                self.balance_ds.addSample(feature, label)
+        self.learning_rate = learning_rate
+        self.balance_num = balance_num
+        self.origin_trainer = BackpropTrainer(self.net, self.origin_ds, learningrate = learning_rate)
+        self.balance_trainer = BackpropTrainer(self.net, self.balance_ds, learningrate = learning_rate)
 
-    def train(self):
-        return self.trainer.train()
+    def origin_train(self):
+        return self.origin_trainer.train()
+
+    def pos_train(self):
+        return self.pos_trainer.train()
+
+    def balance_train(self):
+        return self.balance_trainer.train()
 
     def predicate_score(self, feature_list):
         return [self.net.activate(feature)[0] for feature in feature_list]
@@ -40,9 +65,11 @@ class NeuralNetwork():
         score_list = self.predicate_score(feature_list)
         return [1 if score > 0.5 else 0 for score in score_list]
 
-    def evaluate(self, feature_list, label_list):
-        predicate = numpy.array(self.predicate_label(feature_list))
-        label = numpy.array(label_list)
+    def evaluate(self):
+        predicate = numpy.array(self.predicate_label(self.origin_ds['input']))
+        label = self.origin_ds['target'][:, 0].astype('int32')
+        assert len(predicate.shape) == 1
+        assert len(label.shape) == 1
         positive_true = numpy.logical_and(label, predicate).sum()
         positive_predicate = predicate.sum()
         positive_label = label.sum()
@@ -52,47 +79,26 @@ class NeuralNetwork():
         r = positive_true / positive_label
         if p * r == 0: f1 = 0
         else: f1 = 2 * p * r / (p + r)
-        return p, r, f1
+        return positive_true, positive_predicate, positive_label, p, r, f1
 
 # 特征列表，标签列表
 training_list = pickle.load(open('/home/ezio/filespace/data/training_list.data', 'rb'))
 testing_list = pickle.load(open('/home/ezio/filespace/data/testing_list.data', 'rb'))
-training_feature_list = [sample[3:-1] for sample in training_list]
-training_label_list = [sample[-1] for sample in training_list]
+print('pickle load done')
+feature_count = len(training_list[0]) - 4
+
+feature_list = [sample[3:-1] for sample in training_list]
+label_list = [sample[-1] for sample in training_list]
 testing_feature_list = [sample[3:-1] for sample in testing_list]
 
-# 设置数据集
-feature_count = len(training_feature_list[0])
-all_ds = SupervisedDataSet(feature_count, 1)
-pos_ds = SupervisedDataSet(feature_count, 1)
-neg_ds = SupervisedDataSet(feature_count, 1)
-for x, y in zip(training_feature_list, training_label_list):
-    all_ds.addSample(x, y)
-    if y == 1:
-        pos_ds.addSample(x, y)
-    else:
-        if random.randint(0, 100) == 0: neg_ds.addSample(x, y)
-
-# 设置模型了
-net = NeuralNetwork(feature_count, [100, 40], 1)
-net.set_trainer(pos_ds, 0.001)
-
-
 '''
-print('begin train')
-while True:
-    begin = time.time()
-    pos_trainer.train()
-    neg_trainer.train()
-    # all_trainer.train()
-    end = time.time()
-    print('train time:', end - begin)
-    print('evaluate:')
-    label = numpy.concatenate((pos_ds['target'][:, 0], neg_ds['target'][:, 0])) > 0
-    predicate = numpy.array([net.activate(vec)[0] for vec in pos_ds['input']] + [net.activate(vec)[0] for vec in neg_ds['input']]) > 0
-    print(evaluate(training_feature_list, training_label_list))
-
-
-# trainer.trainUntilConvergence()
-
+clf = svm.SVC()
+clf.fit(feature_list, label_list)
+print('svm fit done')
+predicate_list = clf.predict(feature_list)
 '''
+
+net = NeuralNetwork([100], training_list, 0.001, 30)
+begin = time.time()
+end = time.time()
+print('train time:', end - begin)
